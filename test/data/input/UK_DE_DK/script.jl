cec68f7d57b5f9d8e120588db96009369ccc628a
sdwhardy <steve_dw_hardy@yahoo.ca>
2022-03-30 18:23:49 +0200

    Update script.jl

--- test/data/input/UK_DE_DK/script.jl
+++ test/data/input/UK_DE_DK/script.jl
@@ -7,10 +7,10 @@ import PowerModels; const _PM = PowerModels
 ##################### Input parameters #################################
 rt_ex=pwd()*"\\test\\data\\input\\UK_DE_DK\\"#folder path
 argz = Dict(
-"test"=>false,#if true smallest (2 hour) problem variation is built for testing
+"test"=>true,#if true smallest (2 hour) problem variation is built for testing
 "scenario_planning_horizon"=>30,
 "scenario_names"=>["EU17","EU18","EU19","EU20","ST17","ST18","ST19","ST20","DG17","DG18","DG19","DG20"],#Options: ["EU17","EU18","EU19","EU20","ST17","ST18","ST19","ST20","DG17","DG18","DG19","DG20"]
-"k"=>10,#number of representative days modelled (24 hours per day)//Must add clustered time series for each k
+"k"=>2,#number of representative days modelled (24 hours per day)//Must add clustered time series for each k
 "scenario_years"=>["2020","2030","2040"],#Options: ["2020","2030","2040"]
 "owpp_mva"=>[4000],#mva of wf in MVA
 "conv_lim"=>4000,#Max Converter size in MVA
