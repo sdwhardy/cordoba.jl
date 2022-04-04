8ff6ccd2e75f121efda91eac6355a4f7681db68a
sdwhardy <steve_dw_hardy@yahoo.ca>
2022-04-04 08:55:31 +0200

    admm update

--- test/data/input/UK_DE_DK/script.jl
+++ test/data/input/UK_DE_DK/script.jl
@@ -7,7 +7,7 @@ import PowerModels; const _PM = PowerModels
 ##################### Input parameters #################################
 rt_ex=pwd()*"\\test\\data\\input\\UK_DE_DK\\"#folder path
 argz = Dict(
-"test"=>true,#if true smallest (2 hour) problem variation is built for testing
+"test"=>false,#if true smallest (2 hour) problem variation is built for testing
 "scenario_planning_horizon"=>30,
 "scenario_names"=>["EU17","EU18","EU19","EU20","ST17","ST18","ST19","ST20","DG17","DG18","DG19","DG20"],#Options: ["EU17","EU18","EU19","EU20","ST17","ST18","ST19","ST20","DG17","DG18","DG19","DG20"]
 "k"=>2,#number of representative days modelled (24 hours per day)//Must add clustered time series for each k Available: 2, 5, 10, 50, 100
@@ -26,7 +26,7 @@ s = Dict("output" => Dict("branch_flows" => false),
 "AC"=>"1",#0=false, 1=true
 "eps"=>0.0001,#admm residual (100kW)
 "beta"=>5.5,
-"relax_problem" => false,
+"relax_problem" => true,
 "conv_losses_mp" => false,
 "process_data_internally" => false,
 "corridor_limit" => true,
@@ -60,7 +60,7 @@ _CBD.print_solution_data(result_mip, data, argz)#print MIP solution=#
 s["relax_problem"]=true
 mn_data, data, argz, s = _CBD.main_ACDC_wstrg(rt_ex,argz, s);#Build data structure for given options
 gurobi = JuMP.optimizer_with_attributes(Gurobi.Optimizer,"OutputFlag" => 0)
-result_mip = @time _CBD.admm_4_AjAwAgAuAo_main(mn_data, gurobi, s);#Solve problem
+result_mip = _CBD.admm_4_AjAwAgAuAo_main(mn_data, gurobi, s);#Solve problem
 _CBD.print_solution_data(result_mip, data, argz)#print Convex (ADMM) solution
 
 mn_data, data, s = _CBD.convex2mip(result_mip, data, mn_data, s)#Convert convex to candicate cables
