9c272260bdb3d6264ff2446a7fa73a9cc5f68f3f
sdwhardy <40562565+sdwhardy@users.noreply.github.com>
2022-06-08 13:51:11 +0200

    gap changed

--- test/data/input/UK_DE_DK/script.jl
+++ test/data/input/UK_DE_DK/script.jl
@@ -11,9 +11,9 @@ s = Dict(
 ################# temperal parameters #################
 "test"=>false,#if true smallest (2 hour) problem variation is built for testing
 "scenario_planning_horizon"=>30,
-"scenario_names"=>["NT"],#["NT","DE","GA"]
-"k"=>4,#number of representative days modelled (24 hours per day)//#best for maintaining mean/max is k=6 2014, 2015
-"res_years"=>["2014"],#Options: ["2012","2013","2014","2015","2016"]//#best for maintaining mean/max is k=6 2014, 2015
+"scenario_names"=>["NT","DE","GA"],#["NT","DE","GA"]
+"k"=>6,#number of representative days modelled (24 hours per day)//#best for maintaining mean/max is k=6 2014, 2015
+"res_years"=>["2014","2015"],#Options: ["2012","2013","2014","2015","2016"]//#best for maintaining mean/max is k=6 2014, 2015
 "scenario_years"=>["2020","2030","2040"],#Options: ["2020","2030","2040"]
 "dr"=>0.04,#discount rate
 "yearly_investment"=>1000000,
@@ -45,11 +45,11 @@ _CBD.print_solution_wcost_data(result_mip, s, data)#-856896.0245340846
 results=Dict("result_mip"=>result_mip,"data"=>data, "mn_data"=>mn_data, "s"=>s)
 FileIO.save("C:\\Users\\shardy\\Documents\\julia\\times_series_input_large_files\\UK_DE_DK\\nodal_results.jld2",results)
 
-s["home_market"]=[[1,4]]
+s["home_market"]=[[3,4]]
 @time result_mip, data, mn_data, s = _CBD.zonal_market_main(s);
 _CBD.print_solution_wcost_data(result_mip, s, data)#-856559.087752747 (MIP)
 results=Dict("result_mip"=>result_mip,"data"=>data, "mn_data"=>mn_data, "s"=>s)
-FileIO.save("C:\\Users\\shardy\\Documents\\julia\\times_series_input_large_files\\UK_DE_DK\\zonal_results_hm14.jld2",results)
+FileIO.save("C:\\Users\\shardy\\Documents\\julia\\times_series_input_large_files\\UK_DE_DK\\zonal_results_hm34.jld2",results)
 result_mip["solution"]["nw"]["2592"]["storage"]["3"]
 
 #result_mip=deepcopy(result_mip_001)
