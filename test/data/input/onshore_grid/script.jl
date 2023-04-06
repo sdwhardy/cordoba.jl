03d157673d942bbee3c86670a62e7231f2a52fbe
sdwhardy <steve_dw_hardy@yahoo.ca>
2023-04-06 10:38:23 +0200

    pre branching for market aware paper

--- test/data/input/onshore_grid/script.jl
+++ test/data/input/onshore_grid/script.jl
@@ -9,7 +9,7 @@ s = Dict(
 "rt_ex"=>pwd()*"\\test\\data\\input\\onshore_grid\\",#folder path
 "scenario_data_file"=>"C:\\Users\\shardy\\Documents\\julia\\times_series_input_large_files\\scenario_data_for_UKFRBENLDEDKNO.jld2",
 ################# temperal parameters #################
-"test"=>false,#if true smallest (2 hour) problem variation is built for testing
+"test"=>true,#if true smallest (2 hour) problem variation is built for testing
 "scenario_planning_horizon"=>30,
 "scenario_names"=>["NT","DE","GA"],#["NT","DE","GA"]
 "k"=>4,#number of representative days modelled (24 hours per day)//#best for maintaining mean/max is k=6 2014, 2015
